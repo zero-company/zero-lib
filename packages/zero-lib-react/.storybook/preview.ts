fd65e98443f06cb1e9624b96d6f16610d597ff2b
Jigz <jigzpalillo@gmail.com>
2024-08-09 05:42:17 +0800

    Add CUSTOM_VIEWPORTS

--- packages/zero-lib-react/.storybook/preview.ts
+++ packages/zero-lib-react/.storybook/preview.ts
@@ -1,9 +1,34 @@
 import type { Preview } from '@storybook/react'
 import { themes } from '@storybook/theming'
 import './../src/globals.css'
+import { INITIAL_VIEWPORTS, MINIMAL_VIEWPORTS } from '@storybook/addon-viewport'
+
+const CUSTOM_VIEWPORTS = {
+  kindleFire2: {
+    name: 'Kindle Fire 2',
+    styles: {
+      width: '600px',
+      height: '963px',
+    },
+  },
+  kindleFireHD: {
+    name: 'Kindle Fire HD',
+    styles: {
+      width: '533px',
+      height: '801px',
+    },
+  },
+}
 
 const preview: Preview = {
   parameters: {
+    viewport: {
+      viewports: {
+        ...INITIAL_VIEWPORTS,
+        ...MINIMAL_VIEWPORTS,
+        ...CUSTOM_VIEWPORTS,
+      },
+    },
     docs: {
       theme: themes.dark,
     },
