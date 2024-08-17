01baf1d8f17ccbc43c06ff6339c4e41826f68f01
Jigz <jigzpalillo@gmail.com>
2024-08-18 02:16:17 +0800

    Add portals

--- packages/zero-lib-react/src/app/docs/page.tsx
+++ packages/zero-lib-react/src/app/docs/page.tsx
@@ -1,9 +1,13 @@
 import Link from 'next/link'
 import Docs from '@/components/markdown/docs.mdx'
+import { SidebarPortal } from '@/lib'
 
 export default function Page() {
   return (
     <main className='prose prose-invert'>
+      <SidebarPortal>
+        <p>inside docs portal</p>
+      </SidebarPortal>
       <Link href='/'>Home</Link>
       <Link href='/docs'>Docs</Link>
       <Docs />
--- packages/zero-lib-react/src/app/page.tsx
+++ packages/zero-lib-react/src/app/page.tsx
@@ -1,8 +1,12 @@
 import Link from 'next/link'
+import { SidebarPortal } from '@/lib'
 
 export default function Home() {
   return (
     <main className=''>
+      <SidebarPortal>
+        <p>inside home portal</p>
+      </SidebarPortal>
       <p className='p-2'>HomePage</p>
       <Link href='/'>Home</Link>
       <Link href='/docs'>Docs</Link>
