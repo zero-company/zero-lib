7ed255c033284f73d88fc1c15c5cd239db220fa6
Jigz <jigzpalillo@gmail.com>
2024-08-19 06:56:10 +0800

    Update Sidebar

--- packages/zero-lib-react/src/lib/sidebar/sidebar.tsx
+++ packages/zero-lib-react/src/lib/sidebar/sidebar.tsx
@@ -22,75 +22,52 @@ type Props = {
 
 export const Sidebar = ({ className, header, tabs }: Props) => {
   return (
-    <div className={cn('w-64 flex divide-x', className)}>
-      <div
+    <Tabs
+      defaultValue='1'
+      orientation='vertical'
+      className={cn('w-64 flex flex-row divide-x', className)}
+    >
+      <TabsList
         className={cn(
-          'flex flex-col w-10 overflow-y-auto',
+          'p-0 bg-inherit flex flex-col h-auto',
           HEADER_SIDEBAR_SIZE.W,
-          BORDER_COLOR,
         )}
       >
         {tabs?.top?.map((tab, key) => <span key={key}>{tab.icon}</span>)}
         <div className='flex-1' />
-        <Icon reactIcon={<LuSearch />} />
-        <Icon reactIcon={<LuUserCircle2 />} />
-        <Icon reactIcon={<LuSettings />} />
-      </div>
-
-      <div className={cn('flex-1 flex flex-col divide-y')}>
-        <Tabs
-          defaultValue='1'
-          orientation='vertical'
-          className='flex flex-row divide-x'
-        >
-          <TabsList className='p-0 bg-inherit flex flex-col h-auto'>
-            <TabsTrigger
-              value='1'
-              className='p-0 data-[state=active]:bg-inherit'
-            >
-              <Icon reactIcon={<LuSearch />} />
-            </TabsTrigger>
-            <TabsTrigger
-              value='2'
-              className='p-0 data-[state=active]:bg-inherit'
-            >
-              <Icon reactIcon={<LuUserCircle2 />} />
-            </TabsTrigger>
-            <TabsTrigger
-              value='3'
-              className='p-0 data-[state=active]:bg-inherit'
-            >
-              <Icon reactIcon={<LuSettings />} />
-            </TabsTrigger>
-          </TabsList>
-          <div className='flex-1 flex flex-col divide-y'>
-            <div
-              className={cn('flex divide-x *:h-full', HEADER_SIDEBAR_SIZE.H)}
-            >
-              <div className='flex-1'>
-                {header || (
-                  <div className='p-2'>
-                    <p>zero-lib</p>
-                  </div>
-                )}
+        <TabsTrigger value='1' className='p-0 data-[state=active]:bg-inherit'>
+          <Icon reactIcon={<LuSearch />} />
+        </TabsTrigger>
+        <TabsTrigger value='2' className='p-0 data-[state=active]:bg-inherit'>
+          <Icon reactIcon={<LuUserCircle2 />} />
+        </TabsTrigger>
+        <TabsTrigger value='3' className='p-0 data-[state=active]:bg-inherit'>
+          <Icon reactIcon={<LuSettings />} />
+        </TabsTrigger>
+      </TabsList>
+      <div className='flex-1 flex flex-col divide-y'>
+        <div className={cn('flex divide-x *:h-full', HEADER_SIDEBAR_SIZE.H)}>
+          <div className='flex-1'>
+            {header || (
+              <div className='p-2'>
+                <p>zero-lib</p>
               </div>
-              <Button size='sm' variant='ghost' className='sm:hidden'>
-                <LuChevronRight />
-              </Button>
-            </div>
-            <TabsContent value='1' className='m-0'>
-              1
-            </TabsContent>
-            <TabsContent value='2' className='m-0'>
-              2
-            </TabsContent>
-            <TabsContent value='3' className='m-0'>
-              3
-            </TabsContent>
+            )}
           </div>
-        </Tabs>
-        <div />
+          <Button size='sm' variant='ghost' className='sm:hidden'>
+            <LuChevronRight />
+          </Button>
+        </div>
+        <TabsContent value='1' className='m-0'>
+          1
+        </TabsContent>
+        <TabsContent value='2' className='m-0'>
+          2
+        </TabsContent>
+        <TabsContent value='3' className='m-0'>
+          3
+        </TabsContent>
       </div>
-    </div>
+    </Tabs>
   )
 }
