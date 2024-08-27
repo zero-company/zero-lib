951c98f91a432ad37406af21adbd895f902ee5f9
Jigz <jigzpalillo@gmail.com>
2024-08-27 23:54:35 +0800

    Fix tsconfig conflict

--- packages/zero-lib-react/src/app/docs/page.tsx
+++ packages/zero-lib-react/src/app/docs/page.tsx
@@ -4,8 +4,6 @@ import Docs from '@/components/markdown/docs.mdx'
 export default function Page() {
   return (
     <main className='prose prose-invert'>
-      <Link href='/'>Home</Link>
-      <Link href='/docs'>Docs</Link>
       <Docs />
     </main>
   )
