104caa8dbc33d7b17139d77e35ea8159fae0c0f2
Jigz <jigzpalillo@gmail.com>
2024-09-05 22:05:14 +0800

    Fix radius

---
+++ packages/zero-lib-react/src/lib/nav/nav.tsx
@@ -0,0 +1,16 @@
+"use client"
+import Link from "next/link"
+import { cn } from "@/lib/utils"
+import { buttonVariants } from "@/components/ui/button"
+
+type Props = {
+  
+}
+
+export const Nav = ({}: Props) => {
+  return (
+    <div>
+      <p>nav</p>
+    </div>
+  )
+}
