d836c24c85c4487868db1d3357d5496bd0cb4c43
Jigz <jigzpalillo@gmail.com>
2024-09-06 18:19:24 +0800

    Update radius

--- packages/zero-lib-react/src/lib/support-card/support-card.tsx
+++ packages/zero-lib-react/src/lib/support-card/support-card.tsx
@@ -7,13 +7,13 @@ export const SupportCard = () => {
     <div className='flex p-2 gap-2 '>
       <Button
         size='sm'
-        className='text-xs text-white gap-2 justify-start bg-gradient-to-tr from-mdeeppurple-a400 to-mpurple-a700'
+        className='text-xs text-white gap-2 bg-gradient-to-tr from-mdeeppurple-a400 to-mpurple-a700 flex-1'
       >
         Support Zero
       </Button>
       <Button
         size='sm'
-        className='text-xs text-white gap-2 justify-start bg-gradient-to-tr from-mdeeppurple-a400 to-mblue-a400'
+        className='text-xs text-white gap-2 bg-gradient-to-tr from-mdeeppurple-a400 to-mblue-a400 flex-1'
       >
         Join Community
       </Button>
