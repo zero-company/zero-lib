9265e3a7398822bb381d1895f765e9d580f225c7
Jigz <jigzpalillo@gmail.com>
2024-09-12 19:43:05 +0800

    Add cards

--- packages/zero-lib-react/src/app/zero/about-app/page.tsx
+++ packages/zero-lib-react/src/app/zero/about-app/page.tsx
@@ -1,9 +1,14 @@
 import Link from 'next/link'
+import { FeatureCard } from './cards'
 
 export default function Page() {
   return (
-    <main className=''>
-      <p className='p-2'>About App</p>
+    <main className='flex flex-col p-2 gap-2'>
+      <p className=''>About App</p>
+      <div className='flex gap-2'>
+        <FeatureCard />
+        <FeatureCard />
+      </div>
     </main>
   )
 }
