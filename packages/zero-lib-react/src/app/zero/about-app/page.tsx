8255a40200598bde606bf438bd5f85923676f1cc
Jigz <jigzpalillo@gmail.com>
2024-09-12 21:45:15 +0800

    Update FeatureCard

--- packages/zero-lib-react/src/app/zero/about-app/page.tsx
+++ packages/zero-lib-react/src/app/zero/about-app/page.tsx
@@ -1,15 +1,91 @@
 import Link from 'next/link'
-import { FeatureCard } from './cards'
+import { FeatureCard } from './feature-card'
 
 export default function Page() {
   return (
     <main className='flex flex-col p-2 gap-2'>
       <p className=''>About App</p>
       <div className='flex flex-wrap gap-2'>
-        <FeatureCard title='Centralize' />
-        <FeatureCard title='Control' />
-        <FeatureCard title='Secure' />
-        <FeatureCard title='Manage' />
+        <FeatureCard
+          title='Centralize'
+          features={[
+            {
+              title: 'Strictly Necessary',
+              description:
+                'These cookies are essential in order to use the website and use its features.',
+            },
+            {
+              title: 'Functional Cookies',
+              description:
+                'These cookies allow the website to provide personalized functionality.',
+            },
+            {
+              title: 'Performance Cookies',
+              description:
+                'These cookies help to improve the performance of the website.',
+            },
+          ]}
+        />
+        <FeatureCard
+          title='Control'
+          features={[
+            {
+              title: 'Strictly Necessary',
+              description:
+                'These cookies are essential in order to use the website and use its features.',
+            },
+            {
+              title: 'Functional Cookies',
+              description:
+                'These cookies allow the website to provide personalized functionality.',
+            },
+            {
+              title: 'Performance Cookies',
+              description:
+                'These cookies help to improve the performance of the website.',
+            },
+          ]}
+        />
+        <FeatureCard
+          title='Secure'
+          features={[
+            {
+              title: 'Strictly Necessary',
+              description:
+                'These cookies are essential in order to use the website and use its features.',
+            },
+            {
+              title: 'Functional Cookies',
+              description:
+                'These cookies allow the website to provide personalized functionality.',
+            },
+            {
+              title: 'Performance Cookies',
+              description:
+                'These cookies help to improve the performance of the website.',
+            },
+          ]}
+        />
+        <FeatureCard
+          title='Manage'
+          features={[
+            {
+              title: 'Strictly Necessary',
+              description:
+                'These cookies are essential in order to use the website and use its features.',
+            },
+            {
+              title: 'Functional Cookies',
+              description:
+                'These cookies allow the website to provide personalized functionality.',
+            },
+            {
+              title: 'Performance Cookies',
+              description:
+                'These cookies help to improve the performance of the website.',
+            },
+          ]}
+        />
       </div>
     </main>
   )
