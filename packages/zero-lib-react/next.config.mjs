53f00a932f5da961cfc6e78953898f77416d1f51
Jigz <jigzpalillo@gmail.com>
2024-09-16 08:02:33 +0800

    Update mdx config

--- packages/zero-lib-react/next.config.mjs
+++ packages/zero-lib-react/next.config.mjs
@@ -1,4 +1,5 @@
 import createMDX from '@next/mdx'
+import remarkGfm from 'remark-gfm'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -7,6 +8,11 @@ const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 }
 
-const withMDX = createMDX({})
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+  },
+})
 
 export default withMDX(nextConfig)
