9ca11c631f9fee46b61c652ba8ea6379d5853940
Jigz <jigzpalillo@gmail.com>
2024-09-21 09:24:12 +0800

    Add Menu links

--- packages/zero-lib-react/src/lib/menu/menu.tsx
+++ packages/zero-lib-react/src/lib/menu/menu.tsx
@@ -1,9 +1,10 @@
 'use client'
 import { Button } from '@/components/shadcn'
 import { IconV2 } from '@/lib'
+import Link from 'next/link'
 
 type Option = {
-  name: string
+  children: React.ReactNode
   icon?: React.ReactNode
   disabled?: boolean
   onClick?: () => void
@@ -23,29 +24,38 @@ TODO: Label
 TODO: Notif badge count
 */
 
+const MenuButton = ({ option }: { option: Option }) => (
+  <Button
+    size='sm'
+    className='text-xs gap-2 justify-start h-8 p-2 pl-8 w-full'
+    variant='ghost'
+    disabled={option.disabled}
+    onClick={option.onClick}
+  >
+    {option.icon && (
+      <span className='-ml-6'>
+        <IconV2 reactIcon={option.icon} size='sm' />
+      </span>
+    )}
+    {option.children}
+  </Button>
+)
+
 export const Menu = ({ label, options }: Props) => {
   return (
     <div className='flex flex-col p-1'>
       {label && (
         <p className='text-xs text-muted-foreground pb-1 pl-2'>{label}</p>
       )}
-      {options.map((option, key) => (
-        <Button
-          key={key}
-          size='sm'
-          className='text-xs gap-2 justify-start h-8 p-2 pl-8'
-          variant='ghost'
-          disabled={option.disabled}
-          onClick={option.onClick}
-        >
-          {option.icon && (
-            <span className='-ml-6'>
-              <IconV2 reactIcon={option.icon} size='sm' />
-            </span>
-          )}
-          {option.name}
-        </Button>
-      ))}
+      {options.map((option, key) =>
+        option.onClick ? (
+          <MenuButton key={key} option={option} />
+        ) : (
+          <Link key={key} href={option.href || ''}>
+            <MenuButton option={option} />
+          </Link>
+        ),
+      )}
     </div>
   )
 }
