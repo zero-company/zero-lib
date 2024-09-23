2d02f238eb25a2a4dbd0d3616e6fd6636c3e348b
Jigz <jigzpalillo@gmail.com>
2024-09-23 23:41:04 +0800

    Update GradientButton gradients

--- packages/zero-lib-react/src/lib/button/gradient-button.tsx
+++ packages/zero-lib-react/src/lib/button/gradient-button.tsx
@@ -2,22 +2,29 @@
 import { cn, Button, ButtonProps } from '@/components/shadcn'
 
 const gradients = {
-  deepPurpleToBlue: 'bg-gradient-to-tr from-mdeeppurple-a400 to-mblue-a400',
-  deepPurpleToPurple: 'bg-gradient-to-tr from-mdeeppurple-a400 to-mpurple-a700',
+  purple1: 'bg-gradient-to-tr from-mdeeppurple-a400 to-mblue-a400',
+  purple2: 'bg-gradient-to-tr from-mdeeppurple-a400 to-mpurple-a700',
+  indigo1: 'bg-gradient-to-tr from-indigo-600 to-purple-500',
+  blue1: 'bg-gradient-to-br from-[#868cff] to-[#4318ff]',
+  orange1: 'bg-gradient-to-br from-[#FFC837] to-[#FF6108]',
+  orange2: 'bg-gradient-to-br from-[#FF9966] to-[#FF5E62]',
+  red1: 'bg-gradient-to-br from-[#FF416C] to-[#FF4B2B]',
+  pink1: 'bg-gradient-to-br from-[#EC008C] to-[#FC6767]',
+  pink2: 'bg-gradient-to-br from-[#F857A6] to-[#FF5858]',
 } as const
 
 type Props = {
   size?: ButtonProps['size']
   className?: string
   children: React.ReactNode
-  gradient: keyof typeof gradients
+  gradient?: keyof typeof gradients
 }
 
 export const GradientButton = ({
   children,
   className,
   size = 'sm',
-  gradient = 'deepPurpleToBlue',
+  gradient = 'purple1',
 }: Props) => {
   return (
     children && (
