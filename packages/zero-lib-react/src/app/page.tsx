afc43efd4b663e81eca0b901dbe77cc1e9ba97d6
Jigz <jigzpalillo@gmail.com>
2024-09-28 05:16:07 +0800

    Fix layout links

--- packages/zero-lib-react/src/app/page.tsx
+++ packages/zero-lib-react/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <head>
-        <meta httpEquiv='refresh' content='0; url=/zero/' />
+        <meta httpEquiv='refresh' content='0; url=/app/' />
       </head>
       <main className='flex w-screen h-screen'>
         <SplashPage />
