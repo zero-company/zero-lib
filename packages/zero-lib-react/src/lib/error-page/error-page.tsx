7db1b0d67dd71bf1ca0e816f49b10c2c3b426113
Jigz <jigzpalillo@gmail.com>
2024-10-05 01:26:10 +0800

    Cleanup ErrorPage

--- packages/zero-lib-react/src/lib/error-page/error-page.tsx
+++ packages/zero-lib-react/src/lib/error-page/error-page.tsx
@@ -15,9 +15,7 @@ export const ErrorPage = () => {
     <Page layout='center'>
       <Card className=' max-w-xs m-auto'>
         <CardHeader>
-          <CardTitle className='text-xl'>
-            [---] Unknown
-          </CardTitle>
+          <CardTitle className='text-xl'>[---] Unknown</CardTitle>
           <CardDescription>Unknown Error occurred.</CardDescription>
         </CardHeader>
         <CardContent>
@@ -30,17 +28,4 @@ export const ErrorPage = () => {
       </Card>
     </Page>
   )
-  /*
-   return (
-    <div className='flex w-full h-full justify-center'>
-      <div className='flex max-w-4xl flex-col justify-center'>
-        <h1 className='text-3xl font-bold'>{StatusCodes.NOT_FOUND}</h1>
-        <p className='text-balance text-muted-foreground'>
-          {ReasonPhrases.NOT_FOUND}
-        </p>
-      </div>
-    </div>
-  )
-  
-  */
 }
