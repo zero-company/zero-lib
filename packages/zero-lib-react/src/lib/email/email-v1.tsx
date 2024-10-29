01f332b7fe1a6129168b1a5a44549cecf68bfe8f
Jigz <jigzpalillo@gmail.com>
2024-10-29 13:41:05 +0800

    Add EmailV1 tailwind polyfills

--- packages/zero-lib-react/src/lib/email/email-v1.tsx
+++ packages/zero-lib-react/src/lib/email/email-v1.tsx
@@ -20,7 +20,12 @@ import {
   TailwindConfig,
 } from '@react-email/components'
 
-const twconfig: TailwindConfig = {
+const normalizeCss = `
+/*! modern-normalize v3.0.1 | MIT License | https://github.com/sindresorhus/modern-normalize */
+*,::after,::before{box-sizing:border-box}html{font-family:system-ui,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji';line-height:1.15;-webkit-text-size-adjust:100%;tab-size:4}body{margin:0}b,strong{font-weight:bolder}code,kbd,pre,samp{font-family:ui-monospace,SFMono-Regular,Consolas,'Liberation Mono',Menlo,monospace;font-size:1em}small{font-size:80%}sub,sup{font-size:75%;line-height:0;position:relative;vertical-align:baseline}sub{bottom:-.25em}sup{top:-.5em}table{border-color:currentcolor}button,input,optgroup,select,textarea{font-family:inherit;font-size:100%;line-height:1.15;margin:0}[type=button],[type=reset],[type=submit],button{-webkit-appearance:button}legend{padding:0}progress{vertical-align:baseline}::-webkit-inner-spin-button,::-webkit-outer-spin-button{height:auto}[type=search]{-webkit-appearance:textfield;outline-offset:-2px}::-webkit-search-decoration{-webkit-appearance:none}::-webkit-file-upload-button{-webkit-appearance:button;font:inherit}summary{display:list-item}
+/*# sourceMappingURL=/sm/d2d8cd206fb9f42f071e97460f3ad9c875edb5e7a4b10f900a83cdf8401c53a9.map */
+`
+const twConfig: TailwindConfig = {
   theme: {
     extend: {
       colors: {
@@ -31,13 +36,11 @@ const twconfig: TailwindConfig = {
     },
   },
 }
-
-const normalizeCss = `
-/*! modern-normalize v3.0.1 | MIT License | https://github.com/sindresorhus/modern-normalize */
-*,::after,::before{box-sizing:border-box}html{font-family:system-ui,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji';line-height:1.15;-webkit-text-size-adjust:100%;tab-size:4}body{margin:0}b,strong{font-weight:bolder}code,kbd,pre,samp{font-family:ui-monospace,SFMono-Regular,Consolas,'Liberation Mono',Menlo,monospace;font-size:1em}small{font-size:80%}sub,sup{font-size:75%;line-height:0;position:relative;vertical-align:baseline}sub{bottom:-.25em}sup{top:-.5em}table{border-color:currentcolor}button,input,optgroup,select,textarea{font-family:inherit;font-size:100%;line-height:1.15;margin:0}[type=button],[type=reset],[type=submit],button{-webkit-appearance:button}legend{padding:0}progress{vertical-align:baseline}::-webkit-inner-spin-button,::-webkit-outer-spin-button{height:auto}[type=search]{-webkit-appearance:textfield;outline-offset:-2px}::-webkit-search-decoration{-webkit-appearance:none}::-webkit-file-upload-button{-webkit-appearance:button;font:inherit}summary{display:list-item}
-/*# sourceMappingURL=/sm/d2d8cd206fb9f42f071e97460f3ad9c875edb5e7a4b10f900a83cdf8401c53a9.map */
-`
-const borderT = 'border-t border-solid border-border border-b-0 border-x-0'
+const tw = {
+  // Polyfills to fix lack of base styles, https://github.com/resend/react-email/issues/1615
+  borderT: ' border-solid border-border border-x-0 border-t border-b-0',
+  borderB: 'border-solid border-border border-x-0 border-b border-t-0',
+}
 
 type Props = {
   children: React.ReactNode
@@ -65,15 +68,15 @@ export const EmailV1 = ({ children, preview }: Props) => {
         <style type='text/css'>{normalizeCss}</style>
       </Head>
       {preview && <Preview>{preview}</Preview>}
-      <Tailwind config={twconfig}>
+      <Tailwind config={twConfig}>
         <Body
           className='bg-background text-foreground my-auto mx-auto font-sans p-2'
           style={{}}
         >
           <div className='border border-solid border-border rounded max-w-[465px]'>
-            <Header />
-            <div className={`p-2 ${borderT}`}>{children}</div>
-            <Footer className={`${borderT}`} />
+            <Header className={`${tw.borderB}`} />
+            <div className={`p-2`}>{children}</div>
+            <Footer className={`${tw.borderT}`} />
           </div>
         </Body>
       </Tailwind>
