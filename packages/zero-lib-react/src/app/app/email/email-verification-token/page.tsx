edae0dcda4bac86e4c59bb56ff900acf28d37398
Jigz <jigzpalillo@gmail.com>
2024-10-29 08:00:45 +0800

    Fix email iframe render

--- packages/zero-lib-react/src/app/app/email/email-verification-token/page.tsx
+++ packages/zero-lib-react/src/app/app/email/email-verification-token/page.tsx
@@ -3,15 +3,17 @@ import { renderEmailVerificationTokenEmail } from '@/lib'
 import { useState } from 'react'
 
 export default function Page() {
-  const [html, setHtml] = useState('')
+  const [html, setHtml] = useState<string | null>(null)
   renderEmailVerificationTokenEmail().then(html => setHtml(html))
 
   return (
     <>
-      <div className='flex flex-col bg-white overflow-y-auto'>
+      {console.log(html)}
+      <div className='flex flex-col overflow-y-auto max-w-screen-sm w-full h-[100rem]'>
         <p>Email Verification Token</p>
-        <div dangerouslySetInnerHTML={{ __html: html }} />
+        {html && <iframe className='h-full' srcDoc={html} />}
       </div>
+      <div />
     </>
   )
 }
