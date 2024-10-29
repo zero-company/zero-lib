8cf9aa949fdc7d749b3e14a1fbff1d57fb6154c6
Jigz <jigzpalillo@gmail.com>
2024-10-29 08:15:52 +0800

    Add react-email output textarea

--- packages/zero-lib-react/src/app/app/email/email-verification-token/page.tsx
+++ packages/zero-lib-react/src/app/app/email/email-verification-token/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { renderEmailVerificationTokenEmail } from '@/lib'
+import { renderEmailVerificationTokenEmail, Textarea } from '@/lib'
 import { useState } from 'react'
 
 export default function Page() {
@@ -8,10 +8,10 @@ export default function Page() {
 
   return (
     <>
-      {console.log(html)}
-      <div className='flex flex-col overflow-y-auto max-w-screen-sm w-full h-[100rem]'>
+      <div className='flex flex-col overflow-y-auto max-w-screen-sm w-full'>
         <p>Email Verification Token</p>
-        {html && <iframe className='h-full' srcDoc={html} />}
+        {html && <iframe className='h-[40rem]' srcDoc={html} />}
+        <Textarea className='h-[40rem]' value={html || 'null'} />
       </div>
       <div />
     </>
