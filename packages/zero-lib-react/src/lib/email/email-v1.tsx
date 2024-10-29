311425d4fdd05da6f313efa83922bd4ba4b2c17a
Jigz <jigzpalillo@gmail.com>
2024-10-29 12:56:31 +0800

    Add normalizeCss to EmailV1

--- packages/zero-lib-react/src/lib/email/email-v1.tsx
+++ packages/zero-lib-react/src/lib/email/email-v1.tsx
@@ -32,6 +32,12 @@ const twconfig: TailwindConfig = {
   },
 }
 
+const normalizeCss = `
+/*! modern-normalize v3.0.1 | MIT License | https://github.com/sindresorhus/modern-normalize */
+*,::after,::before{box-sizing:border-box}html{font-family:system-ui,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji';line-height:1.15;-webkit-text-size-adjust:100%;tab-size:4}body{margin:0}b,strong{font-weight:bolder}code,kbd,pre,samp{font-family:ui-monospace,SFMono-Regular,Consolas,'Liberation Mono',Menlo,monospace;font-size:1em}small{font-size:80%}sub,sup{font-size:75%;line-height:0;position:relative;vertical-align:baseline}sub{bottom:-.25em}sup{top:-.5em}table{border-color:currentcolor}button,input,optgroup,select,textarea{font-family:inherit;font-size:100%;line-height:1.15;margin:0}[type=button],[type=reset],[type=submit],button{-webkit-appearance:button}legend{padding:0}progress{vertical-align:baseline}::-webkit-inner-spin-button,::-webkit-outer-spin-button{height:auto}[type=search]{-webkit-appearance:textfield;outline-offset:-2px}::-webkit-search-decoration{-webkit-appearance:none}::-webkit-file-upload-button{-webkit-appearance:button;font:inherit}summary{display:list-item}
+/*# sourceMappingURL=/sm/d2d8cd206fb9f42f071e97460f3ad9c875edb5e7a4b10f900a83cdf8401c53a9.map */
+`
+
 type Props = {
   children: React.ReactNode
   preview?: string
@@ -39,28 +45,33 @@ type Props = {
 
 const Header = () => (
   <div className='flex'>
-    <p>Header</p>
-    <p>Logo</p>
+    <span>Header</span>
+    <span>Logo</span>
   </div>
 )
 
 const Footer = () => (
   <div className='flex'>
-    <p>Footer</p>
-    <p>Socials</p>
+    <span>Footer</span>
+    <span>Socials</span>
   </div>
 )
 
 export const EmailV1 = ({ children, preview }: Props) => {
   return (
     <Html lang='en'>
-      <Head />
+      <Head>
+        <style type='text/css'>{normalizeCss}</style>
+      </Head>
       {preview && <Preview>{preview}</Preview>}
       <Tailwind config={twconfig}>
-        <Body className='bg-background text-foreground my-auto mx-auto font-sans p-2'>
-          <Container className='border border-solid border-border rounded max-w-[465px] p-2'>
+        <Body
+          className='bg-background text-foreground my-auto mx-auto font-sans p-2'
+          style={{}}
+        >
+          <Container className='border border-solid border-border rounded max-w-[465px]'>
             <Header />
-            {children}
+            <div className='p-2'>{children}</div>
             <Footer />
           </Container>
         </Body>
