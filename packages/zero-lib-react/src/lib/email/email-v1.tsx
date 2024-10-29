a8547e8fd234032b034da0164aa92de1115034ec
Jigz <jigzpalillo@gmail.com>
2024-10-29 15:45:17 +0800

    Add EmailV1 logo

--- packages/zero-lib-react/src/lib/email/email-v1.tsx
+++ packages/zero-lib-react/src/lib/email/email-v1.tsx
@@ -19,11 +19,11 @@ import {
   Markdown,
   TailwindConfig,
 } from '@react-email/components'
+import { ZeroLogo } from '../zero/zero-logo'
 
-const normalizeCss = `
 /*! modern-normalize v3.0.1 | MIT License | https://github.com/sindresorhus/modern-normalize */
+const normalizeCss = `
 *,::after,::before{box-sizing:border-box}html{font-family:system-ui,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji';line-height:1.15;-webkit-text-size-adjust:100%;tab-size:4}body{margin:0}b,strong{font-weight:bolder}code,kbd,pre,samp{font-family:ui-monospace,SFMono-Regular,Consolas,'Liberation Mono',Menlo,monospace;font-size:1em}small{font-size:80%}sub,sup{font-size:75%;line-height:0;position:relative;vertical-align:baseline}sub{bottom:-.25em}sup{top:-.5em}table{border-color:currentcolor}button,input,optgroup,select,textarea{font-family:inherit;font-size:100%;line-height:1.15;margin:0}[type=button],[type=reset],[type=submit],button{-webkit-appearance:button}legend{padding:0}progress{vertical-align:baseline}::-webkit-inner-spin-button,::-webkit-outer-spin-button{height:auto}[type=search]{-webkit-appearance:textfield;outline-offset:-2px}::-webkit-search-decoration{-webkit-appearance:none}::-webkit-file-upload-button{-webkit-appearance:button;font:inherit}summary{display:list-item}
-/*# sourceMappingURL=/sm/d2d8cd206fb9f42f071e97460f3ad9c875edb5e7a4b10f900a83cdf8401c53a9.map */
 `
 const twConfig: TailwindConfig = {
   theme: {
@@ -40,6 +40,8 @@ const tw = {
   // Polyfills to fix lack of base styles, https://github.com/resend/react-email/issues/1615
   borderT: ' border-solid border-border border-x-0 border-t border-b-0',
   borderB: 'border-solid border-border border-x-0 border-b border-t-0',
+  borderL: 'border-solid border-border border-y-0 border-l border-r-0',
+  borderR: 'border-solid border-border border-y-0 border-r border-l-0',
 }
 
 type Props = {
@@ -48,16 +50,18 @@ type Props = {
 }
 
 const Header = ({ className }: { className?: string }) => (
-  <div className={`flex ${className}`}>
-    <span>Header</span>
-    <span>Logo</span>
+  <div className={`flex h-10 ${className}`}>
+    <div className={`flex-1 p-2 ${tw.borderR}`}>Header</div>
+    <div className={`h-10 w-10 p-2.5`}>
+      <ZeroLogo />
+    </div>
   </div>
 )
 
 const Footer = ({ className }: { className?: string }) => (
-  <div className={`flex ${className}`}>
-    <span>Footer</span>
-    <span>Socials</span>
+  <div className={`flex h-10 ${className}`}>
+    <div className={`flex-1 p-2 ${tw.borderR}`}>Footer</div>
+    <div className={`p-2`}>Socials</div>
   </div>
 )
 
