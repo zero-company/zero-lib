c2a3ec9973da35cfb09752e5bf796f60ed75c314
Jigz <jigzpalillo@gmail.com>
2024-11-12 14:42:31 +0800

    Add zod-schema uuid

--- packages/zero-lib/src/zod-schemas/zod-schemas.ts
+++ packages/zero-lib/src/zod-schemas/zod-schemas.ts
@@ -1,12 +1,30 @@
 import { z } from 'zod'
 import { parseZeroId } from '../zero-id/zero-id'
+import { v4 as uuidV4, validate as uuidValidate } from 'uuid'
+
+const uuidZodSchemas = {
+  uuid: z.string().uuid().nullish(),
+  uuidRequired: z.string().uuid(),
+  uuidArray: z.string().uuid().array(),
+  uuidGeneratedOnly: z
+    .null()
+    .or(z.undefined())
+    .transform(zeroId => zeroId ?? uuidV4()),
+  uuidGeneratedOrCustom: z
+    .string()
+    .uuid()
+    .nullish()
+    .transform(zeroId => zeroId ?? uuidV4()),
+}
 
 export const zodSchemas = {
+  ...uuidZodSchemas,
   name: z.string().min(1, { message: 'Empty string not Allowed' }).nullish(),
   description: z
     .string()
     .min(1, { message: 'Empty string not Allowed' })
     .nullish(),
+
   zeroId: z
     .string()
     .nullish()
