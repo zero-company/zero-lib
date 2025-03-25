4cb9c5ff3fb20204230ec3765932c837942425fe
Jigz <jigzpalillo@gmail.com>
2025-03-25 20:44:22 +0800

    Use inline gradient colors

--- packages/zero-lib-react/src/lib/colors.ts
+++ packages/zero-lib-react/src/lib/colors.ts
@@ -1,7 +1,7 @@
 export const GRADIENTS = {
-  purple1: 'bg-gradient-to-tr from-mdeeppurple-a400 to-mblue-a400',
-  purple2: 'bg-gradient-to-tr from-mdeeppurple-a400 to-mpurple-a700',
-  indigo1: 'bg-gradient-to-tr from-indigo-600 to-purple-500',
+  purple1: 'bg-gradient-to-tr from-[#651FFF] to-[#2979FF]',
+  purple2: 'bg-gradient-to-tr from-[#651FFF] to-[#AA00FF]',
+  indigo1: 'bg-gradient-to-tr from-[#4f46e5] to-[#a855f7]',
   blue1: 'bg-gradient-to-br from-[#868cff] to-[#4318ff]',
   orange1: 'bg-gradient-to-br from-[#FFC837] to-[#FF6108]',
   orange2: 'bg-gradient-to-br from-[#FF9966] to-[#FF5E62]',
