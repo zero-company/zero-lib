7227e752b62c8c398d5f7724375849341334ca3b
Jigz <jigzpalillo@gmail.com>
2025-04-05 20:36:54 +0800

    Split AppSidebarV2

--- packages/zero-lib-react/src/lib/app-sidebar/app-sidebar-v2.tsx
+++ packages/zero-lib-react/src/lib/app-sidebar/app-sidebar-v2.tsx
@@ -194,11 +194,7 @@ const data = {
   ],
 }
 
-type Props = {
-  sidebarGroups?: React.ReactNode
-}
-
-export const AppSidebarV2 = ({ sidebarGroups }: Props) => {
+export const AppSidebarV2Header = () => {
   const [activeTeam, setActiveTeam] = useState(data.teams[0])
 
   if (!activeTeam) {
@@ -206,64 +202,152 @@ export const AppSidebarV2 = ({ sidebarGroups }: Props) => {
   }
 
   return (
-    <Sidebar className='*:bg-(--background)' collapsible='icon'>
-      <SidebarHeader>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <SidebarMenuButton
-                  size='lg'
-                  className='data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground'
+    <SidebarHeader>
+      <SidebarMenu>
+        <SidebarMenuItem>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <SidebarMenuButton
+                size='lg'
+                className='data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground'
+              >
+                <div className='flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground'>
+                  <activeTeam.logo className='size-4' />
+                </div>
+                <div className='grid flex-1 text-left text-sm leading-tight'>
+                  <span className='truncate font-semibold'>
+                    {activeTeam.name}
+                  </span>
+                  <span className='truncate text-xs'>{activeTeam.plan}</span>
+                </div>
+                <ChevronsUpDown className='ml-auto' />
+              </SidebarMenuButton>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              className='w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg'
+              align='start'
+              side='bottom'
+              sideOffset={4}
+            >
+              <DropdownMenuLabel className='text-xs text-muted-foreground'>
+                Teams
+              </DropdownMenuLabel>
+              {data.teams.map((team, index) => (
+                <DropdownMenuItem
+                  key={team.name}
+                  onClick={() => setActiveTeam(team)}
+                  className='gap-2 p-2'
                 >
-                  <div className='flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground'>
-                    <activeTeam.logo className='size-4' />
+                  <div className='flex size-6 items-center justify-center rounded-sm border'>
+                    <team.logo className='size-4 shrink-0' />
                   </div>
+                  {team.name}
+                  <DropdownMenuShortcut>⌘{index + 1}</DropdownMenuShortcut>
+                </DropdownMenuItem>
+              ))}
+              <DropdownMenuSeparator />
+              <DropdownMenuItem className='gap-2 p-2'>
+                <div className='flex size-6 items-center justify-center rounded-md border bg-background'>
+                  <Plus className='size-4' />
+                </div>
+                <div className='font-medium text-muted-foreground'>
+                  Add team
+                </div>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </SidebarMenuItem>
+      </SidebarMenu>
+    </SidebarHeader>
+  )
+}
+
+export const AppSidebarV2Footer = () => {
+  return (
+    <SidebarFooter>
+      <SidebarMenu>
+        <SidebarMenuItem>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <SidebarMenuButton
+                size='lg'
+                className='data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground'
+              >
+                <Avatar className='h-8 w-8 rounded-lg'>
+                  <AvatarImage src={data.user.avatar} alt={data.user.name} />
+                  <AvatarFallback className='rounded-lg'>CN</AvatarFallback>
+                </Avatar>
+                <div className='grid flex-1 text-left text-sm leading-tight'>
+                  <span className='truncate font-semibold'>
+                    {data.user.name}
+                  </span>
+                  <span className='truncate text-xs'>{data.user.email}</span>
+                </div>
+                <ChevronsUpDown className='ml-auto size-4' />
+              </SidebarMenuButton>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              className='w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg'
+              side='bottom'
+              align='end'
+              sideOffset={4}
+            >
+              <DropdownMenuLabel className='p-0 font-normal'>
+                <div className='flex items-center gap-2 px-1 py-1.5 text-left text-sm'>
+                  <Avatar className='h-8 w-8 rounded-lg'>
+                    <AvatarImage src={data.user.avatar} alt={data.user.name} />
+                    <AvatarFallback className='rounded-lg'>CN</AvatarFallback>
+                  </Avatar>
                   <div className='grid flex-1 text-left text-sm leading-tight'>
                     <span className='truncate font-semibold'>
-                      {activeTeam.name}
+                      {data.user.name}
                     </span>
-                    <span className='truncate text-xs'>{activeTeam.plan}</span>
+                    <span className='truncate text-xs'>{data.user.email}</span>
                   </div>
-                  <ChevronsUpDown className='ml-auto' />
-                </SidebarMenuButton>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent
-                className='w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg'
-                align='start'
-                side='bottom'
-                sideOffset={4}
-              >
-                <DropdownMenuLabel className='text-xs text-muted-foreground'>
-                  Teams
-                </DropdownMenuLabel>
-                {data.teams.map((team, index) => (
-                  <DropdownMenuItem
-                    key={team.name}
-                    onClick={() => setActiveTeam(team)}
-                    className='gap-2 p-2'
-                  >
-                    <div className='flex size-6 items-center justify-center rounded-sm border'>
-                      <team.logo className='size-4 shrink-0' />
-                    </div>
-                    {team.name}
-                    <DropdownMenuShortcut>⌘{index + 1}</DropdownMenuShortcut>
-                  </DropdownMenuItem>
-                ))}
-                <DropdownMenuSeparator />
-                <DropdownMenuItem className='gap-2 p-2'>
-                  <div className='flex size-6 items-center justify-center rounded-md border bg-background'>
-                    <Plus className='size-4' />
-                  </div>
-                  <div className='font-medium text-muted-foreground'>
-                    Add team
-                  </div>
+                </div>
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem>
+                  <Sparkles />
+                  Upgrade to Pro
                 </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarHeader>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem>
+                  <BadgeCheck />
+                  Account
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <CreditCard />
+                  Billing
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Bell />
+                  Notifications
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <LogOut />
+                Log out
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </SidebarMenuItem>
+      </SidebarMenu>
+    </SidebarFooter>
+  )
+}
+
+type Props = {
+  sidebarGroups?: React.ReactNode
+}
+export const AppSidebarV2 = ({ sidebarGroups }: Props) => {
+  return (
+    <Sidebar className='*:bg-(--background)' collapsible='icon'>
+      <AppSidebarV2Header />
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupLabel>Platform</SidebarGroupLabel>
@@ -350,85 +434,7 @@ export const AppSidebarV2 = ({ sidebarGroups }: Props) => {
           </SidebarMenu>
         </SidebarGroup>
       </SidebarContent>
-      <SidebarFooter>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <SidebarMenuButton
-                  size='lg'
-                  className='data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground'
-                >
-                  <Avatar className='h-8 w-8 rounded-lg'>
-                    <AvatarImage src={data.user.avatar} alt={data.user.name} />
-                    <AvatarFallback className='rounded-lg'>CN</AvatarFallback>
-                  </Avatar>
-                  <div className='grid flex-1 text-left text-sm leading-tight'>
-                    <span className='truncate font-semibold'>
-                      {data.user.name}
-                    </span>
-                    <span className='truncate text-xs'>{data.user.email}</span>
-                  </div>
-                  <ChevronsUpDown className='ml-auto size-4' />
-                </SidebarMenuButton>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent
-                className='w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg'
-                side='bottom'
-                align='end'
-                sideOffset={4}
-              >
-                <DropdownMenuLabel className='p-0 font-normal'>
-                  <div className='flex items-center gap-2 px-1 py-1.5 text-left text-sm'>
-                    <Avatar className='h-8 w-8 rounded-lg'>
-                      <AvatarImage
-                        src={data.user.avatar}
-                        alt={data.user.name}
-                      />
-                      <AvatarFallback className='rounded-lg'>CN</AvatarFallback>
-                    </Avatar>
-                    <div className='grid flex-1 text-left text-sm leading-tight'>
-                      <span className='truncate font-semibold'>
-                        {data.user.name}
-                      </span>
-                      <span className='truncate text-xs'>
-                        {data.user.email}
-                      </span>
-                    </div>
-                  </div>
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuGroup>
-                  <DropdownMenuItem>
-                    <Sparkles />
-                    Upgrade to Pro
-                  </DropdownMenuItem>
-                </DropdownMenuGroup>
-                <DropdownMenuSeparator />
-                <DropdownMenuGroup>
-                  <DropdownMenuItem>
-                    <BadgeCheck />
-                    Account
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <CreditCard />
-                    Billing
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <Bell />
-                    Notifications
-                  </DropdownMenuItem>
-                </DropdownMenuGroup>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                  <LogOut />
-                  Log out
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarFooter>
+      <AppSidebarV2Footer />
       <SidebarRail />
     </Sidebar>
   )
