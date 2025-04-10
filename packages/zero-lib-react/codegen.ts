b971cd60dcfb71de9e3deb9f8d77bf5a60419685
Jigz <jigzpalillo@gmail.com>
2025-04-10 13:53:06 +0800

    Update codegen

--- packages/zero-lib-react/codegen.ts
+++ packages/zero-lib-react/codegen.ts
@@ -1,4 +1,6 @@
 import type { CodegenConfig } from '@graphql-codegen/cli'
+import 'dotenv/config'
+import { z } from 'zod'
 
 const config: CodegenConfig = {
   schema: 'https://graphql.org/graphql/',
@@ -12,6 +14,25 @@ const config: CodegenConfig = {
       plugins: ['schema-ast'],
       config: {
         includeDirectives: true,
+      },
+    },
+    './src/query/graphql-validation.ts': {
+      plugins: ['typescript', 'typescript-validation-schema'],
+      config: {
+        // strictScalars: true,
+        useEnumTypeAsDefaultValue: true,
+        defaultScalarSchema: z.unknown(),
+        scalarSchemas: {
+          Date: 'z.string().datetime()',
+          DateTime: 'z.string().datetime()',
+        },
+        scalars: {
+          ID: 'string',
+          DateTime: 'string',
+          Date: 'string',
+          JSON: '{ [key: string]: any }',
+        },
+        schema: 'zod',
       },
     },
   },
