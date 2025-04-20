77bed8f98027aba9be4a4c279ce01ba36bfb2255
Jigz <jigzpalillo@gmail.com>
2025-04-20 20:17:44 +0800

    Update table page

--- packages/zero-lib-react/src/app/app/table/page.tsx
+++ packages/zero-lib-react/src/app/app/table/page.tsx
@@ -1,9 +1,15 @@
 'use client'
+import { UsersTable } from './_components/users-table'
 
 export default function Page() {
+  const promises = Promise.all([])
+
   return (
-    <>
+    <div>
       <p>table</p>
-    </>
+      <UsersTable
+      // promises={promises}
+      />
+    </div>
   )
 }
